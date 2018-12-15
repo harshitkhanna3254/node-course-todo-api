f03ab0b14bc9bac87b5c56a3b5088910b168ac94
Harshit Khanna <harshitkhanna3254@gmail.com>
2018-12-15 16:15:08 +0530

    Set up app for heroku

--- server/server.js
+++ server/server.js
@@ -4,12 +4,13 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos', (req,res) => {
-    console.log(req.body);
     var todo = new Todo({
         text: req.body.text
     })
@@ -19,9 +20,29 @@ app.post('/todos', (req,res) => {
     }, (error) => {
         res.send(error)
     })
+})
 
+app.get('/todos', (req, res) => {
+    Todo.find().then( (docs) => {
+        res.send(docs)
+    }, (e) => {res.send(e)})
+})
+
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id))
+       return res.send("ID not a valid ObjectID")
+
+    Todo.findById(id).then( (doc) => {
+        if(!doc)
+            return res.send("No document found")
+            else
+                res.send({todo: doc})
+    }, (e) => {res.send(e)})
 })
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000")
+    console.log(`Listening on port ${port}`)
 });
