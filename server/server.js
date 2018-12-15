623c6c1a33019329a974e9cc4fb6ee7e6b86bafc
Harshit Khanna <harshitkhanna3254@gmail.com>
2018-12-16 04:08:43 +0530

    Added PATCh /todos:id. Was kinda difficult. Added JSON.parse() to convert JSON data from client to Javascript object. Took me the whole day

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
 var app = express();
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos', (req,res) => {
+    console.log(typeof(req.body.text));
     var todo = new Todo({
         text: req.body.text
     })
@@ -56,8 +58,35 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((error) => {
         res.send(error)
     })
-
 })
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    console.log(typeof(JSON.parse(req.body.completed)));
+    var body = _.pick(req.body, ['text', 'completed']);
+    console.log(typeof(body.completed));
+
+    if(!ObjectID.isValid(id))
+        return res.send("Invalid Object ID");
+
+        console.log( JSON.parse(body.completed));
+
+    if(_.isBoolean(JSON.parse(body.completed)) && JSON.parse(body.completed)){
+        body.completedAt = new Date().getTime();
+    } else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then( (doc) => {
+        if(!doc)
+            return res.send("No todo of that id found")
+        res.send({todo: doc})
+    }).catch((err) => {
+        res.send("Some error occured")
+    });
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
